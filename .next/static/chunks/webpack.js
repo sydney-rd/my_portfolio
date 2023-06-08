2190bbb701837012bf08ad1f8179f3645410130d
sydney-rd <sydneydavid56@gmail.com>
2023-06-08 17:05:59 -0400

    updating syntax

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "98acba4beedaad34"; }
+/******/ 		__webpack_require__.h = function() { return "b5e231d5ae0fd422"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/projectPage.js
+++ pages/projectPage.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import { projects } from '../utilities/projects'
-import { Flex, Box, VStack, Link as ChakraLink } from '@chakra-ui/react'
-import Link from 'next/link'
-import { motion } from 'framer-motion'
-import ProjectCategories from '../components/ProjectCategories'
+import React, { useState } from "react"
+import { projects } from "../utilities/projects"
+import { Flex, Box, VStack, Link as ChakraLink } from "@chakra-ui/react"
+import Link from "next/link"
+import { motion } from "framer-motion"
+import ProjectCategories from "../components/ProjectCategories"
 
 const MotionChakraLink = motion(ChakraLink)
 
 export default function ProjectPage() {
-  const [selectedCategory, setSelectedCategory] = useState('WEB')
-  const [hoveredItem, setHoveredItem] = useState('')
-  const [tint, setTint] = useState({ color: '', opacity: '0' })
-
+  const [selectedCategory, setSelectedCategory] = useState("WEB")
+  const [hoveredItem, setHoveredItem] = useState("")
+  const [tint, setTint] = useState({ color: "", opacity: "0" })
 
 
   const LinkItem = ({
@@ -34,15 +33,15 @@ export default function ProjectPage() {
         href={href}
         target={target}
         sx={{
-          opacity: '1',
-          fontFamily: 'Ailerons',
-          fontSize: '9rem',
-          whiteSpace: 'nowrap',
-          textAlign: 'right',
-          transition: 'none',
-          filter: 'brightness(150%)',
-          color: isHovered ? color : 'transparent',
-          WebkitTextStroke: '2px',
+          opacity: "1",
+          fontFamily: "Ailerons",
+          fontSize: "9rem",
+          whiteSpace: "nowrap",
+          textAlign: "right",
+          transition: "none",
+          filter: "brightness(150%)",
+          color: isHovered ? color : "transparent",
+          WebkitTextStroke: "2px",
           WebkitTextStrokeColor: color,
           _hover: { color: color, textShadow: `0px 0px 8px ${color}` }
         }}
@@ -63,41 +62,41 @@ export default function ProjectPage() {
 
   const getBackgroundImage = () => {
     switch (selectedCategory) {
-      case 'WEB':
+      case "WEB":
         return "url('https://i.pinimg.com/originals/90/09/ec/9009ecc79e92b8ba207b43c7e56bfb41.gif')"
-      case 'C++':
+      case "C++":
         return "url('https://rare-gallery.com/mocahbig/434051-road-mist-dark-night-rain-nature.jpg')"
-      case 'PYTHON':
+      case "PYTHON":
         return "url('https://i.pinimg.com/474x/5b/df/93/5bdf937fbc9174341ce59c72be420db6.jpg')"
-      case 'FULLSTACK':
+      case "FULLSTACK":
         return "url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/40cab845-8738-49c2-aef8-25770e405eae/dd9hxuf-ea275591-d328-4302-bc74-54151cde0cd4.png/v1/fill/w_1600,h_686,q_80,strp/the_academy___rooftop___blackout_by_tamagochikun_dd9hxuf-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9Njg2IiwicGF0aCI6IlwvZlwvNDBjYWI4NDUtODczOC00OWMyLWFlZjgtMjU3NzBlNDA1ZWFlXC9kZDloeHVmLWVhMjc1NTkxLWQzMjgtNDMwMi1iYzc0LTU0MTUxY2RlMGNkNC5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.RN2GkbNr8jUxKJkiMiZYodYtUnrm0dW9xHi-A6NZfWk')"
       default:
-        return ''
+        return ""
     }
   }
 
   const hover = (color, text) => {
     if (color) {
-      setTint({ color, opacity: '0.4' })
+      setTint({ color, opacity: "0.4" })
       setHoveredItem(text)
     }
   }
 
   const hoverLeave = () => {
     setTint({
-      opacity: '0',
-      color: ''
+      opacity: "0",
+      color: ""
     })
-    setHoveredItem('')
+    setHoveredItem("")
   }
 
 
   const backgroundStyle = {
     backgroundImage: getBackgroundImage(),
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    minHeight: '100vh',
-    width: '100vw',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    minHeight: "100vh",
+    width: "100vw",
     transition: "opacity 0.7s"
   }
 
